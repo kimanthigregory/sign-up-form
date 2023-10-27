4ad013b0f33714b9e3aabedd8ba7f87ec0b541b5
Greg <gregorykimanthi100@gmail.com>
2023-10-28 00:29:28 +0300

    worked on form validation but am for the first input element

---
+++ index.js
@@ -0,0 +1,51 @@
+const errorMessage = document.querySelector(".form p");
+const inputBox = document.querySelectorAll(".input-field");
+const submitButton = document.querySelector("#submit");
+
+const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+function inputValidation(){
+    
+}
+submitButton.addEventListener("click", (event)=>{
+    // alert("error");
+    event.preventDefault();
+    let index =0
+    inputBox.forEach(function(input,index) {
+        const checkInputField = input.value.trim() === "";
+        if (checkInputField){
+
+            console.log(input);
+            customError(index,input)
+        }
+        else{
+            alert("hello")
+        }  
+        index++;
+    });
+    
+})
+function customError(key,input){
+    switch (key) {
+        case 0 :
+            var paragraph = input.closest(".input-container");
+            input.classList.add("show-error-icon");
+            var par =paragraph.querySelector("p");
+            par.classList.add("show-error-message");
+            console.log(par);
+            break;
+        case 1 :
+            alert("one")
+            break;
+        case 2 :
+            alert("two")
+            break;
+        case 3 :
+            alert("three")
+            break;
+
+        default:
+            alert("error message");
+            break;
+    }
+}
