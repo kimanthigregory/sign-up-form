74f7c5cb212a290276d8081d1ca52aaa0fc206dd
Greg <gregorykimanthi100@gmail.com>
2023-10-28 22:52:34 +0300

    The form validation still needs some work

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
 const errorMessage = document.querySelector(".form p");
 const inputBox = document.querySelectorAll(".input-field");
-const submitButton = document.querySelector("#submit");
+const inputArea = document.querySelector(".input-field");
+const submitButton = document.querySelector("#submitt");
 
 const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 function inputValidation(){
     
 }
+inputBox.forEach(function(input,index) {
+    
+    inputArea.addEventListener("input", ()=>{
+        const checkInputField = input.value.trim() === "";
+
+        if (checkInputField){
+            console.log(index);
+            customError(index,input)
+        }
+        else{
+            console.log(index);
+            input.classList.add("hide-error-icon");
+            var paragraph = input.closest(".input-container");
+            var par =paragraph.querySelector("p");
+            par.classList.add("hide-error-message");
+        }
+       
+    })
+   
+    
+});
 submitButton.addEventListener("click", (event)=>{
     // alert("error");
     event.preventDefault();
     let index =0
+    var validated= true;
     inputBox.forEach(function(input,index) {
         const checkInputField = input.value.trim() === "";
         if (checkInputField){
-
-            console.log(input);
+            // console.log(input);
             customError(index,input)
+            validated=false;
         }
         else{
-            alert("hello")
-        }  
+      
+            input.classList.add("hide-error-icon");
+            var paragraph = input.closest(".input-container");
+            var par =paragraph.querySelector("p");
+            par.classList.add("hide-error-message");
+           
+
+        }
         index++;
+       
+        
     });
+    if(validated ){
+        document.querySelector("form").submit();
+      
+    }
     
+    
+ 
 })
 function customError(key,input){
     switch (key) {
